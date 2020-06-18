27cb29662e80e77eacb4afe16b72e7a79e16b5ea
daproclaima <sn.itpro@gmail.com>
2020-06-18 17:26:50 +0200

    feat: updates navigation.js and list-movies.js

--- src/components/home/list-movies.js
+++ src/components/home/list-movies.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 
 import {
   Card,
@@ -25,16 +26,82 @@ Movie.propTypes = {
   movie: PropTypes.string.isRequired,
 };
 
-const ListMovies = ({ data }) => (
-  <div>
-    <Row>
-      {data.map((movie) => <Movie movie={movie} />)}
-    </Row>
-  </div>
-);
+class ListMovies extends Component {
+  constructor() {
+    super();
+    this.params = {
+      api_key: 'd50eac545c84928041a50ccd2cb4945e',
+      language: 'en-US',
+      sort_by: 'popularity.desc',
+      include_adult: false,
+      include_video: false,
+      page: 1,
+    };
+    this.state = {
+      data: [],
+      isLoading: false,
+    };
+  }
 
-ListMovies.propTypes = {
-  data: PropTypes.string.isRequired,
-};
+  componentDidMount() {
+    axios.get('https://api.themoviedb.org/3/discover/movie', {
+      params: this.params,
+    }).then((res) => {
+      this.setState({ data: res.data, isLoading: true });
+    });
+  }
+
+  toggleView(theme) {
+    axios.get('https://api.themoviedb.org/3/discover/$%7Btheme%7D%60', {
+      params: theme,
+    })
+      .then((res) => {
+        this.setState({ data: res.data, isLoading: true });
+      });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { input } = this.state;
+    console.log(input);
+    const paramsSubmit = this.params;
+    delete paramsSubmit.include_video;
+    delete paramsSubmit.sort_by;
+    paramsSubmit.query = input;
+    axios.get('https://api.themoviedb.org/3/search/movie', {
+      params: paramsSubmit,
+    })
+      .then((res) => {
+        console.log(res);
+        this.setState({ data: res.data, isLoading: true });
+      });
+  }
+
+  render() {
+    const { data, isLoading } = this.state;
+    if (isLoading) {
+      return (
+        <div>
+          <Row>
+            {data.map((movie) => <Movie movie={movie} />)}
+          </Row>
+        </div>
+      );
+    }
+
+    return (
+      <div>
+        <Row>
+          <Movie movie="chargement" />
+        </Row>
+      </div>
+    );
+  }
+}
+
+// ListMovies.propTypes = {
+//   data: PropTypes.string.isRequired,
+//   isLoading: PropTypes.bool,
+// };
 
 export default ListMovies;
--- src/components/home/navigation.js
+++ src/components/home/navigation.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
-import { Nav, Navbar } from 'react-bootstrap';
+import {
+  Nav,
+  Navbar,
+  Form,
+  FormControl,
+  Button,
+} from 'react-bootstrap';
 
 const Navigation = () => (
   <Navbar bg="dark" variant="dark">
@@ -10,6 +16,12 @@ const Navigation = () => (
       <Nav.Link href="#features">Features</Nav.Link>
       <Nav.Link href="#pricing">Pricing</Nav.Link>
     </Nav>
+    <Form inline>
+      <Button variant="primary" /* onClick={() => this.toggleView('movie')} */ >Movies</Button>
+      <Button variant="primary" /* onClick={() => this.toggleView('tv')} */>Series</Button>
+      <FormControl type="text" idName="nav-search" placeholder="Search" className="mr-sm-2" /* value={input} onChange={(event) => this.handleChange(event)} */ />
+      <Button type="submit" variant="outline-success" /* onClick={(event) => this.handleSubmit(event)} */ >Search</Button> 
+    </Form>
   </Navbar>
 );
 
